b02ec4488cb1590eb747a2826bb8a33d7c4ddd5b
JamesEnoch <JamesEnoch89@gmail.com>
2022-01-27 23:51:28 -0500

    add in sql used

---
+++ EmployeePayRoll/SqlScripts/EmployePayroll.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE DeductionType (      
+Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      
+DeductionName VARCHAR(20) NOT NULL,    
+DeductionCode VARCHAR(5) NOT NULL,
+DeductionAmount DECIMAL(10,2) NOT NULL,        
+)      
+GO 
+
+CREATE TABLE Employee (      
+Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      
+Name VARCHAR(20) NOT NULL,     
+PayPerPeriod DECIMAL(10,2) NOT NULL,
+DeductionTypeId INT NOT NULL FOREIGN KEY REFERENCES DeductionType(Id)                 
+)      
+GO 
+
+CREATE TABLE [Dependent] (      
+Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      
+Name VARCHAR(20) NOT NULL,        
+EmployeeId INT NOT NULL FOREIGN KEY REFERENCES Employee(Id),
+DeductionTypeId INT NOT NULL FOREIGN KEY REFERENCES DeductionType(Id)          
+)      
+GO 
+
+
+INSERT INTO DeductionType (DeductionName, DeductionCode, DeductionAmount)
+VALUES
+('Standard Dependent', 'STNDT', 500),
+('Standard Employee', 'STND', 1000),
+('Special Employee', 'SPE', 500),
+('Special Dependent', 'SPD', 450)
